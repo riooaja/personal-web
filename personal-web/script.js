1931d6220bbaefc8bcf2a0bb55f84bc7bc698b8b
riooaja <169070095+riooaja@users.noreply.github.com>
2026-02-19 16:38:07 +0700

    Delete personal-web directory

--- personal-web/script.js
+++
@@ -1,9 +0,0 @@
-// Tambahkan interaktivitas jika diperlukan
-// Contoh: animasi sederhana saat halaman dimuat
-document.addEventListener('DOMContentLoaded', function() {
-    document.querySelector('.container').style.opacity = 0;
-    setTimeout(function() {
-        document.querySelector('.container').style.transition = 'opacity 1s';
-        document.querySelector('.container').style.opacity = 1;
-    }, 200);
-});
